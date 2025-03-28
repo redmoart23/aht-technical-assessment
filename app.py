5af71dbcfeeaa049b7286e9370c0a4cda05c87ca
redmo academy <108553638+redmoart23@users.noreply.github.com>
2025-03-28 17:32:31 -0500

    ♻️Refactor: Flask application structure and add inventory routes with templates (#2)

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template
+from routes.routes import inventory
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
- 
+app.register_blueprint(inventory)
